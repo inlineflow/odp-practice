aafbb4719214b3feb520795f69ac0e4fd53e196b
inlineflow <thrashdev@protonmail.com>
2025-05-12 14:26:43 +0600

    added shit

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const form = document.querySelector("form");
 
 form.addEventListener("input", (event) => {
   const validations = getValidations();
-  const id = event.target.getAttribute("id");
+  const elementId = event.target.getAttribute("id");
+  const validationIndex = parseId(elementId);
+  console.log(elementId);
 
-  const validation = validations[id];
-  const error = validation(event.target.value);
-  const errorField = document.querySelector(`#${id} + span.error`);
-  processError(errorField, error);
+  const validation = validations[validationIndex];
+  if (validation) {
+    const error = validation(event.target.value);
+    const errorField = document.querySelector(`#${elementId} + span.error`);
+    processError(errorField, error);
+  } else {
+    alert(`no validation for element with id: ${elementId}`);
+  }
 });
 
 // inputs.forEach((i) => console.log(i.getAttribute("id")));
@@ -23,6 +29,20 @@ function getValidations() {
         : "Please provide a valid email address, example : example@gmail.com";
       //   return value;
     },
+    country: (value) => {
+      const countryCodes = ["US", "RU", "KG", "GB", "DE"];
+
+      return countryCodes.includes(value)
+        ? ``
+        : `Not a recognized country code. Must be one of the following ${countryCodes.join(
+            " "
+          )}`;
+    },
+    postalCode: (value) => {
+      return isNumeric(value) ? "" : "Only digits allowed in Postal Code";
+    },
+    password: (value) => validatePassword(value),
+    passwordConfirmation: (value) => validatePassword(value),
   };
   return validations;
 }
@@ -36,4 +56,45 @@ function processError(field, error) {
 
   field.textContent = error;
   field.classList.remove("invisible");
+}
+
+function parseId(id) {
+  const pieces = id.split("-");
+  if (pieces.length === 1) {
+    return pieces[0];
+  }
+  if (pieces.length > 2) {
+    alert("Id of an input form may contain only 2 pieces");
+  }
+  const result =
+    pieces[0] + pieces[1].charAt(0).toUpperCase() + pieces[1].slice(1);
+
+  return result;
+  //   return id.charAt(0).toUpperCase() + id.slice(1);
+}
+
+function isNumeric(s) {
+  return !isNaN(s - parseFloat(s));
+}
+
+function validatePassword() {
+  const passwordField = document.getElementById("password");
+  const passwordConfirmationField = document.getElementById(
+    "password-confirmation"
+  );
+
+  const password = passwordField.value;
+  const passwordConfirmation = passwordConfirmationField.value;
+
+  if (password !== passwordConfirmation) {
+    const error = "Both passwords must match";
+    const passwordErrorField = document.querySelector(`#password + span.error`);
+    const passwordConfErrorField = document.querySelector(
+      `#password-confirmation + span.error`
+    );
+
+    processError(passwordErrorField, error);
+    processError(passwordConfErrorField, error);
+    return error;
+  }
 }
